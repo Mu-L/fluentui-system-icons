8bc3f21d93f35770b8fd2fb9b6b815f7f36824d1
Martin Hochel <martinhochel@microsoft.com>
2026-03-31 16:54:11 +0200

    feat(docsite): enable fui sb addon and port css from v9 for coherent UI (#1033)

--- packages/docsite/.storybook/main.ts
+++ packages/docsite/.storybook/main.ts
@@ -1,9 +1,14 @@
 import type { StorybookConfig } from '@storybook/react-webpack5';
 
 const config: StorybookConfig = {
-  stories: ['../stories/**/*.stories.@(js|jsx|ts|tsx)', '../stories/**/*.mdx'],
+  stories: ['../stories/**/index.stories.@(ts|tsx)', '../stories/**/*.mdx'],
 
-  addons: ['@storybook/addon-docs', '@storybook/addon-webpack5-compiler-swc'],
+  addons: [
+    '@storybook/addon-docs',
+    '@storybook/addon-webpack5-compiler-swc',
+    '@fluentui/react-storybook-addon',
+    '@fluentui/react-storybook-addon-export-to-sandbox',
+  ],
 
   framework: {
     name: '@storybook/react-webpack5',
